46cba989d14913bc78a297ff4ff2de1a3ef47f5c
Luu Duc Trung <luuductrung1234@gmail.com>
2020-02-26 08:55:38 +0700

    beanshell sample

---
+++ CaseStudy/AutomationTesting/learn-jmeter-5/simple-library/src/com/ldt/Main.java
@@ -0,0 +1,11 @@
+package com.ldt;
+
+import com.ldt.helpers.SimpleHelpers;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleHelpers helpers = new SimpleHelpers();
+
+        helpers.printHelloWorld();
+    }
+}
---
+++ CaseStudy/AutomationTesting/learn-jmeter-5/simple-library/src/com/ldt/helpers/SimpleHelpers.java
@@ -0,0 +1,14 @@
+package com.ldt.helpers;
+
+public class SimpleHelpers {
+    public SimpleHelpers() {
+    }
+
+    public int sum(int left, int right) {
+        return left + right;
+    }
+
+    public void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
