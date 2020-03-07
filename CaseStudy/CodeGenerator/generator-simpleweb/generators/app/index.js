42bbfdb62bb7a0edac726add2217c657aeeaf8c3
Luu Duc Trung <luuductrung1234@gmail.com>
2020-03-07 08:54:09 +0700

    update generator-simpleweb

--- CaseStudy/CodeGenerator/generator-simpleweb/generators/app/index.js
+++ CaseStudy/CodeGenerator/generator-simpleweb/generators/app/index.js
@@ -1,4 +1,4 @@
-var Generator = require('yeoman-generator');
+var Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
     constructor(args, opts) {
@@ -6,14 +6,40 @@ module.exports = class extends Generator {
         super(args, opts);
 
         // Next, add your custom code
-        this.option('babel'); // This method adds support for a `--babel` flag
+        this.option("babel"); // This method adds support for a `--babel` flag
     }
 
     onStart() {
-        this.log('Starting...');
+        this.log("Starting...");
     }
 
-    onEnd() {
-        this.log('Ending...');
+    onRunning() {
+        this.prompt([
+            {
+                type: "input",
+                name: "name",
+                message:
+                    "Enter a name for the new component (i.e.: myNewComponent): "
+            }
+        ]).then(answers => {
+            // create destination folder
+            this.destinationRoot(answers.name);
+
+            this.fs.copyTpl(
+                this.templatePath('index.html'),
+                this.destinationPath(answers.name + '.html'),
+                {
+                    title: answers.name,
+                    message: 'Hello World!'
+                }
+            );
+
+            this.fs.copyTpl(
+                this.templatePath('style.css'),
+                this.destinationPath('style.css')
+            );
+
+            this.log("Ending...");
+        });
     }
 };
